4fd6c426fd948bf7b6651ab452e7ad1d112fa984
Hussain-786 <62205945+Hussain-786@users.noreply.github.com>
2020-10-23 21:44:27 +0530

    Updates

---
+++ precache-manifest.f7092573dc987fdc06a3e768c8ebaff2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5eba1bef9261857175de1a0d537669b",
+    "url": "/Covid19-cases/index.html"
+  },
+  {
+    "revision": "4c46fe5d0d746cafe48d",
+    "url": "/Covid19-cases/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "4c46fe5d0d746cafe48d",
+    "url": "/Covid19-cases/static/js/2.6ca30ac4.chunk.js"
+  },
+  {
+    "revision": "6bbb3a8e7360a6a98672414629b231d4",
+    "url": "/Covid19-cases/static/js/2.6ca30ac4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acf7ed8f38f9c09e08da",
+    "url": "/Covid19-cases/static/js/main.e9dd50b7.chunk.js"
+  },
+  {
+    "revision": "756eb0cb1767ea990ccf",
+    "url": "/Covid19-cases/static/js/runtime-main.f3b11502.js"
+  }
+]);
